61453fd95c500c80b49e2ab965a1011809e43cee
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-05 20:30:12 +0300

    ft

--- js/index.js
+++ js/index.js
@@ -8,3 +8,22 @@ days[4] = "Wedsday"
 days[5] = "Thursday"
 days[6] = "Friday"
 
+
+function getDay(){
+    
+    var dd = parseInt(document.getElementById('day').value);
+    console.log(dd);
+    var mm = parseInt(document.getElementById('month').value);
+    console.log(mm);
+    var year = parseInt(document.getElementById('year').value);
+    console.log(year);
+    var cc = parseInt(document.getElementById('year').value.slice(0, 2));
+    console.log(cc);
+    var yy = parseInt(document.getElementById('year').value.slice(2, 4));
+    console.log(yy);
+    var gender = document.getElementById('gender').value;
+    console.log(gender);
+
+
+
+}
