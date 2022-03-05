06ab45ce9c567cbecd4b329bd0d1fe00db93fe67
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-05 18:39:38 +0300

    ft:initializing array - initialize days of the week array

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Initialize Days of the week array
+const days = [];
+days[0] = "Saturday"
+days[1] = "Sunday"
+days[2] = "Monday"
+days[3] = "Tuesday"
+days[4] = "Wedsday"
+days[5] = "Thursday"
+days[6] = "Friday"
+
